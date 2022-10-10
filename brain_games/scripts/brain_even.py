d120bbc74510a8f7e27f8c4749a4d3dc86136c7e
Andrey <volk000@gmail.com>
2022-10-10 08:46:19 +0600

    Fix brain-even

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 from brain_games.greet import greet_user
-from brain_games.engine import generate_even
-from brain_games.games.even import play_game
+from brain_games.engine import play_game
+from brain_games.games.even import generate_even
 
 
 def main():
     GAME_NAME = 'Answer "yes" if the number is even, otherwise answer "no".'
     user_name = greet_user(GAME_NAME)
-    questions_and_answers = play_game()
-    generate_even(user_name, questions_and_answers)
+    questions_and_answers = generate_even()
+    play_game(user_name, questions_and_answers)
 
 
 if __name__ == '__main__':
